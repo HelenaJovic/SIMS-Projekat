c8dd1bdd10e6ffd1fd5c247e8a4ce73cdbb1139f
anabb55 <75089113+anabb55@users.noreply.github.com>
2023-04-05 00:34:28 +0200

    Some changes

---
+++ InitialProject/InitialProject/Model/Accommodation.cs
@@ -0,0 +1,92 @@
+using InitialProject.Domain.Model;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{
+	public class Accommodation : ISerializable
+	{
+
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int IdLocation { get; set; }
+
+		public Location Location { get; set; }
+
+		public AccommodationType Type { get; set; }
+
+		public int MaxGuestNum { get; set; }
+
+		public int MinReservationDays { get; set; }
+
+		public int DaysBeforeCancel { get; set; }
+
+		public List<Image> Images	{get; set;}
+
+		public int IdUser { get; set; }
+
+
+
+		public Accommodation(string name,int idLocation, Location location, AccommodationType type, int maxGuestNum, int minResevationDays, int daysBeforeCancel, int idUser)
+
+		{
+			this.Name = name;
+			this.IdLocation = idLocation;
+			this.Location = location;
+		    this.Type = type;
+			this.MaxGuestNum = maxGuestNum;
+			this.MinReservationDays=minResevationDays;
+			this.DaysBeforeCancel = daysBeforeCancel;
+		    this.IdUser= idUser;
+
+
+
+		}
+
+		public Accommodation()
+		{
+
+		}
+
+		public void FromCSV(string[] values)
+		{
+			Id = int.Parse(values[0]);
+			Name = values[1];
+            IdLocation=int.Parse(values[2]);
+			Type = (AccommodationType)Enum.Parse(typeof(AccommodationType), values[3]);
+			MaxGuestNum = int.Parse(values[4]);
+			MinReservationDays = int.Parse(values[5]);
+			DaysBeforeCancel=int.Parse(values[6]);
+            IdUser = int.Parse(values[7]);
+
+
+		}
+
+
+		public string[] ToCSV()
+		{
+			string[] csvValues =
+			{
+				Id.ToString(),
+				Name,
+				IdLocation.ToString(),
+				Type.ToString(),
+				MaxGuestNum.ToString(),
+				MinReservationDays.ToString(),
+				DaysBeforeCancel.ToString(),
+                IdUser.ToString()
+			    
+
+
+			};
+			return csvValues;
+		}
+
+	}
+}
