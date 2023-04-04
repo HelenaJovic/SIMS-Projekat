95410e6eded828f6f00ee9ea9637a45e5abafe39
ninak235 <116868070+ninak235@users.noreply.github.com>
2023-04-04 22:42:22 +0200

    forgot folder name

--- InitialProject/InitialProject/Applications/UseCases/TourPointService.cs
+++ InitialProject/InitialProject/Applications/UseCases/TourPointService.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InitialProject.UseCases
+namespace Applications.InitialProject.UseCases
 {
     class TourPointService
     {
--- InitialProject/InitialProject/Applications/UseCases/TourService.cs
+++ InitialProject/InitialProject/Applications/UseCases/TourService.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InitialProject.UseCases
+namespace Applications.InitialProject.UseCases
 {
     class TourService
     {
