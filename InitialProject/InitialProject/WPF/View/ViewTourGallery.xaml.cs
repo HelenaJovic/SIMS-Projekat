dbf14b825fa75e1f9f5cf031b5c3e3c75f7b880f
ninak235 <116868070+ninak235@users.noreply.github.com>
2023-04-09 11:49:38 +0200

    commands added

--- InitialProject/InitialProject/WPF/View/ViewTourGallery.xaml.cs
+++ InitialProject/InitialProject/WPF/View/ViewTourGallery.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using InitialProject.Domain.Model;
 using InitialProject.Repository;
+using InitialProject.WPF.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,43 +23,10 @@ namespace InitialProject.View
     /// </summary>
     public partial class ViewTourGallery : Window
     {
-        public static Tour SelectedTour { get; set; }
-
-
-        private readonly ImageRepository _imageRepository;
-
-        public static List<String> ImageUrls = new List<String>();
         public ViewTourGallery(Tour selectedTour)
         {
             InitializeComponent();
-            DataContext = this;
-            SelectedTour = selectedTour;
-            _imageRepository = new ImageRepository();
-            ImageUrls = new List<String>(_imageRepository.GetUrlByTourId(SelectedTour.Id));
-
-
-            DisplayPictures();
-
-        }
-
-        private void DisplayPictures()
-        {
-
-            foreach (String url in ImageUrls)
-            {
-
-                Image image = new Image();
-                BitmapImage bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.UriSource = new Uri(url, UriKind.Relative);
-                bitmapImage.EndInit();
-                image.Source = bitmapImage;
-                image.Width = 130;
-                image.Height = 130;
-                image.Margin = new Thickness(20, 0, 10, 20);
-                WrapPanel wrapPanel = (WrapPanel)FindName("ImagesPanel");
-                wrapPanel.Children.Add(image);
-            }
+            DataContext = new ViewTourGalleryViewModel(selectedTour);
         }
     }
 }
